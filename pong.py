aadd76fb2f73202116f2cb0add8c49fafcfb3c0f
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-09-27 18:24:44 -0300

    pong

--- pong.py
+++ pong.py
@@ -9,25 +9,37 @@ Um exemplo de como o Grid System funciona:
 """
 
 
-def drawBoxFull(scr, x=0, y=0, dx=1, dy=1, ch="*"):
-    """ Draw a full box
-    Arguments:
-        x, y    - start position (int)
-        dx, dy  - length and height of the box (int)
-        ch      - character to draw the box (str)
-    """
-    for j in range(y, dy+y):
-        # O "dx + y + 3" é por causa do Grid System, as medidas são maiores para x do que para y
-        for i in range(x, dx+(dx-1)+x, 2):
-            scr.addstr(j, i, ch)
-
 def main(stdscr):
-    cols, rows = stdscr.getmaxyx()
-    stdscr.clear()
-    drawBoxFull(stdscr, 3, 3+cols//2, dx=3, dy=6)
-    drawBoxFull(stdscr, rows-9, 3+cols//2, dx=3, dy=6)
-    stdscr.refresh()
-    stdscr.getkey()
+
+    def drawBox(scr, x=0, y=0, dx=1, dy=1, ch="*"):
+        """ Draw a full box
+        Arguments:
+            scr     - screen (window) to draw
+            x, y    - start position (int)
+            dx, dy  - length and height of the box (int)
+            ch      - character to draw the box (str)
+        """
+        for j in range(y, dy+y):
+            # O "dx + y + 3" é por causa do Grid System, as medidas são maiores para x do que para y
+            for i in range(x, dx+(dx-1)+x, 2):
+                scr.addstr(j, i, ch)
+
+    def onUpdateFrame(*args):
+        stdscr.getkey()
+
+    def onRenderFrame(*args):
+        cols, rows = stdscr.getmaxyx()
+        stdscr.clear()
+        drawBox(stdscr, 3, (cols//2)-6, 3, 6)
+        drawBox(stdscr, rows-9, (cols//2)-5, 3, 6)
+        drawBox(stdscr, rows//2, (cols//2)-3, 2, 2)
+        stdscr.refresh()
+    onRenderFrame()
+    onUpdateFrame()
+
 
 if __name__ == "__main__":
+    game = True
     wrapper(main)
+    while (game):
+        main()
