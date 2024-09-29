7f03ec8dc0204de152e6ae88307aa73084bf0531
Nícolas Perez <nicolau.sobreira@gmail.com>
2024-09-29 17:48:31 -0300

    29/09/24 17:48

--- pong-class.py
+++ pong-class.py
@@ -1,6 +1,34 @@
 import curses
 from time import sleep
 
+TICKRATE = 50
+
+def drawBox(stdscr, x=0, y=0, sx=1, sy=1, ch="*"):
+    """ Draw a full box
+    Arguments:
+        scr     - screen (window) to draw
+        x, y    - start position (int)
+        sx, sy  - length and height of the box (int)
+        ch      - character to draw the box (str)
+    """
+    for j in range(y, sy+y):
+        for i in range(x, sx+x, 2):
+            stdscr.addstr(j, i, ch)
+
+class Player():
+    def __init__(self, x, y, sx, sy):
+        self.x_player = x
+        self.y_player = y
+        
+        # Tamanho
+        self.sx_player = sx
+        self.sy_player = sy
+        
+        # Velocidade
+        self.v_player = 1
+    
+    # def collision(self):
+
 class Game:
     def __init__(self):
         # Inicia a janela
@@ -10,7 +38,6 @@ class Game:
         curses.noecho()
         curses.curs_set(0)
         
-        # Colunas e filas todais
         self.cols, self.rows = self.stdscr.getmaxyx()
         self.state = True
         
@@ -19,62 +46,102 @@ class Game:
         self.sy_bola = 2
         
         # Velocidade da bola
-        self.vx_bola = 10
-        self.vy_bola = 10
+        self.vx_bola = 1
+        self.vy_bola = 1
         
         # Coordenada da bola
         self.x_bola = self.rows//2
         self.y_bola = (self.cols//2)-3
         
+        # Tamanho dos players
+        self.sx_player = self.sx_bola
+        self.sy_player = self.sy_bola * 3
+        
+        # Velocidade dos players
+        self.v_player = 1
+        
+        # Coordenada dos players
+        self.x_player1 = 0
+        self.y_player1 = self.cols//2
+        self.x_player2 = self.rows - self.sx_player
+        self.y_player2 = self.cols//2
+        
+        # Pontos
+        self.score1 = 0
+        self.score2 = 0
+        
         # Input
         self.key = ""
 
-        
-    def drawBox(self, x=0, y=0, sx=1, sy=1, ch="*"):
-        """ Draw a full box
-        Arguments:
-            scr     - screen (window) to draw
-            x, y    - start position (int)
-            sx, sy  - length and height of the box (int)
-            ch      - character to draw the box (str)
-        """
-        for j in range(y, sy+y):
-            for i in range(x, sx+x, 2):
-                self.stdscr.addstr(j, i, ch)
+    def resetBall(self):
+        self.x_bola = self.rows//2
+        self.y_bola = (self.cols//2)-3
+
+    def printScore(self):
+        self.stdscr.addstr()
 
     def onUpdateFrame(self):
-        sleep(0.25)
+        self.x_bola += self.vx_bola
+        self.y_bola += self.vy_bola
         
-        # Colisão direita e esquerda
-        if self.x_bola + self.vx_bola < self.rows and self.x_bola + self.vx_bola > 0:
-            self.x_bola += self.vx_bola
-        else:
+        if self.x_bola < self.x_player2 - self.sx_player:
             self.vx_bola = -self.vx_bola
-
-        # Colisão cima e baixo
-        if self.y_bola - self.vy_bola > 0 and self.y_bola + self.vy_bola < self.cols:
-            self.y_bola += self.vy_bola
-        else:
+        
+        # Colisão da bola
+        # Direita
+        if self.x_bola + self.sx_bola < self.rows:
+            self.score1 += 1
+            self.resetBall()
+        
+        # Esquerda
+        if self.x_bola - self.sx_bola > 0:
+            self.score2 += 1
+            self.resetBall()
+        
+        # Cima
+        if self.y_bola - self.sy_bola > 0:
             self.vy_bola = -self.vy_bola
-
+        
+        # Baixo
+        if self.y_bola + self.sy_bola < self.cols:
+            self.vy_bola = -self.vy_bola
+        
+        # Input
         self.key = self.stdscr.getch()
-        try:
-            self.key=chr(self.key)
-        except:
-            pass
-        if self.key == "q":
+        
+        # Player 1
+        if self.key == 119: # w
+            self.y_player1 -= self.v_player
+        if self.key == 115: # s
+            self.y_player1 += self.v_player
+
+        # Player 2
+        if self.key == curses.KEY_UP:
+            self.y_player2 -= self.v_player
+        if self.key == curses.KEY_DOWN:
+            self.y_player2 += self.v_player
+        
+        # Game
+        if self.key == 113: # q
             self.state = False
     
     def onRenderFrame(self):
-        # stdscr.clear()
-        self.drawBox(self.x_bola, self.y_bola, self.sx_bola, self.sy_bola)
-        self.stdscr.addstr(5, 9, f"Key: {self.key}")
+        self.stdscr.erase()
+        drawBox(self.stdscr, self.x_bola, self.y_bola, self.sx_bola, self.sy_bola)
+        drawBox(self.stdscr, self.x_player1, self.y_player1, self.sx_player, self.sy_player)
+        drawBox(self.stdscr, self.x_player2, self.y_player2, self.sx_player, self.sy_player)
+        # self.stdscr.addstr(5, 9, f"Key: {self.key}")
+        # self.printScore()
         self.stdscr.refresh()
    
     def main(self, stdscr):
         while self.state:
             self.onUpdateFrame()
             self.onRenderFrame()
+            
+            # Faz com que os inputs não se "arrastem"
+            curses.flushinp()
+            curses.napms(TICKRATE)
 
 if __name__ == "__main__":
     game = Game()
